b78df60cbb94323b140121614b18904856eaa117
INOVITE <Inovite.services@gmail.com>
2025-05-01 05:14:32 +0000

    vite changes

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      input: '/index.html', // Only this exists
+    },
+  },
+  server: {
+    historyApiFallback: true, // 🔥 key to make routes like /home work
+  },
+});
